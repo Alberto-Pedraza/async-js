18ddd53b2845010fa4a1a471edd771f87a0c282e
Alberto Pedraza <washburnuser@gmail.com>
2026-09-04 16:33:49 -0600

    Moví los archivos a donde debían ir, se conectaron el env y la api.service y se hicieron los ejemplos del get

---
+++ env.js
@@ -0,0 +1,6 @@
+/**
+ * Vamos a intentar simular un archivo .env que guarda información sensible como las llaves de acceso o enlaces a APIs
+ * 
+ */
+//La notación con puras mayúsculas en las declaraciones de variables es para cuando son constantes que no van a cambiar para n
+export const API_URL = "https://fakestoreapi.com/products"
---
+++ services/api.service.js
@@ -0,0 +1,32 @@
+/**
+ * Un servicio es un tipo de archivo donde vamos a tener funciones o clases con sus métodos que ejecutan acciones.
+ * 
+ * ! SUPER IMPORTANTE:
+ * ! Todas las funciones asíncronas retornan una promesa. No importa qué pienses tú que está retornando. ES UNA PROMESA
+ */
+
+import { API_URL } from "../env.js";
+
+//GET
+export async function getAllProducts() {
+    //1. Lanzar la petición
+    const response = await fetch(API_URL);
+    const data = await response.json()
+    return data;//Aunque se tenga este return de un array, lo que vamos a ver si llamamos a la función va a ser una promesa, porque es asíncrona
+}
+//getAllProducts();
+
+//Get 1 solo producto
+export const getSingleProduct = async function (id) {
+    try{
+        const response = await fetch(`${API_URL}/${id}`);
+        const data = await response.json();
+        console.log(data);
+    }catch(error){
+        console.error("Error",error);
+    }
+};
+
+getSingleProduct(5);
+getSingleProduct(6);
+getSingleProduct(7);
