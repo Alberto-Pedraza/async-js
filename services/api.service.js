e2df1e0be2bc7cfeb4e2cdad7798bd0e07bc772c
Alberto Pedraza <washburnuser@gmail.com>
2026-09-05 03:00:11 -0600

    Se agregan las últimas dos funciones para poder hacer PUT y DELETE

--- services/api.service.js
+++ services/api.service.js
@@ -7,7 +7,8 @@
 
 import { API_URL } from "../env.js";
 
-//GET
+//*GET
+
 export async function getAllProducts() {
     //1. Lanzar la petición
     const response = await fetch(API_URL);
@@ -26,7 +27,64 @@ export const getSingleProduct = async function (id) {
         console.error("Error",error);
     }
 };
-
+/**
 getSingleProduct(5);
 getSingleProduct(6);
 getSingleProduct(7);
+*/
+
+//*POST
+
+export const createProduct = async (productObject) => {
+    try{
+        const response = await fetch(API_URL,{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(productObject),
+        });
+        const data = await response.json();
+        console.log(data);
+    }catch(error) {}
+};
+
+createProduct({
+title: "crema corporal",
+price: 80,
+description: "Crema alpura",
+category: "Cremas",
+image: "http://example.com"
+});
+
+//* PUT
+
+export async function modifyProduct(id,productObject) {
+    try{
+        const response = await fetch(`${API_URL}/${id}`, {
+            method: 'PUT',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(productObject)
+        });
+        const data = await response.json();
+        console.log(data);
+    }catch(error){}
+}
+
+modifyProduct(5,{
+"title": "Carteras",
+"price": 123456789,
+});
+
+//* DELETE
+export const deleteProduct = async function (id) {
+    try{
+        const response = await fetch(`${API_URL}/${id}`,{
+            method:"DELETE"
+        });
+        const data = await response.json();
+        console.log(data);
+    }catch(error){
+        console.error("Error",error);
+    }
+};
+
+deleteProduct(8);
